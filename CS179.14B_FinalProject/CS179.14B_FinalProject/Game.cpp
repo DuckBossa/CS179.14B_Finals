da4f531bd7dc19129de0e421747e216d7c5ced19
Raphael <DuckBossa@gmail.com>
2016-02-29 01:15:00 +0800

    got texture to show on window. remember: debug libs for debug version. kappapride...

--- CS179.14B_FinalProject/CS179.14B_FinalProject/Game.cpp
+++ CS179.14B_FinalProject/CS179.14B_FinalProject/Game.cpp
@@ -9,8 +9,16 @@
 #include <Windows.h>
 
 
-#define FPS 60.0
-#define SPF sf::seconds(1.0/FPS)
+#define FPS 60.0f
+#define SPF sf::seconds(1.0f/FPS)
+
+const auto TILE_SIZE = 32.0f;
+const auto WINDOW_WIDTH = 800;
+const auto WINDOW_HEIGHT = 600;
+const float GRAV = 9.8f / FPS;
+
+using namespace std;
+
 
 namespace Keys {
 	const auto UP = 0x1A;     	// W
@@ -19,11 +27,6 @@ namespace Keys {
 	const auto RIGHT = 0x07; 	// D
 }
 
-const auto TILE_SIZE = 32.0f;
-const auto WINDOW_WIDTH = 800;
-const auto WINDOW_HEIGHT = 600;
-
-using namespace std;
 
 bool isKeyDown(int key) {
 	auto state = GetAsyncKeyState(MapVirtualKey(key, MAPVK_VSC_TO_VK_EX));
@@ -31,67 +34,39 @@ bool isKeyDown(int key) {
 }
 
 class Entity {
+
 public:
-	virtual void update() = 0;
+	virtual void update(float dt) = 0;
 	virtual void render(sf::RenderTarget &g) = 0;
-//	virtual sf::Vector2f getPosition() const = 0;
+	virtual sf::Rect<float> bounds() = 0;
+	void move(const sf::Vector2f &amount) {
+		pos += amount;
+	}
+	void setPos(const sf::Vector2f &newPos) {
+		pos = newPos;
+	}
+	//virtual sf::Vector2f getPosition() const = 0;
 	virtual ~Entity() {}
 
 protected:
 	sf::Vector2f pos;
 	sf::Vector2i s_size;
+	sf::RectangleShape rect;
+	string tex;
 };
 
-class EntityManager {
-public:
-	std::vector<Entity*> map;
-	std::vector<Entity*> players;
-	std::vector<Entity*> sobjects;
 
-	void addPlayer(Entity* p) {
-		players.push_back(p);
-	}
-
-	void addMapTile(Entity* t) {
-		map.push_back(t);
-	}
-
-	void addSObject(Entity* so) {
-		sobjects.push_back(so);
-	}
-
-
-	void update() {
-		//
-	}
-
-	void logic() {
-		//collision
-	}
-
-	void render(sf::RenderTarget &g) {
-		for (auto e : map) {
-			e->render(g);
-		}
-		for (auto e : players) {
-			e->render(g);
-		}
-		for (auto e : sobjects) {
-			e->render(g);
-		}
-	}
-
-
-}em;
 
 
 class Tile : public Entity {
 protected:
-	sf::RectangleShape rect;
+
 public:
-	Tile(float size, const sf::Color &color,const sf::Vector2f &init_pos) :rect(sf::Vector2f(size, size)) {
+	Tile(float size, const sf::Color &color, const sf::Vector2f &init_pos) {
+		rect = sf::RectangleShape(sf::Vector2f(size, size));
 		rect.setFillColor(color);
-		rect.setPosition(init_pos);
+		rect.setPosition(init_pos + sf::Vector2f(size / 2.0f, size / 2.0f));
+		rect.setOrigin(sf::Vector2f(size / 2.0f, size / 2.0f));
 		pos = init_pos;
 	}
 
@@ -99,23 +74,57 @@ public:
 		g.draw(rect);
 	}
 	void virtual DoSomethingOnCollision(Entity* collided) = 0;
+
+
+	void resolveColision(Entity* collided) {
+		sf::Rect<float> inter;
+		if (collided->bounds().intersects(bounds(), inter)) {
+			sf::Rect<float> temp_bounds = collided->bounds();
+			if (pos.y > inter.top) {
+				//getAnimatedSprite().move(0,axis.y*((other.top + other.height) - inter.top));
+				collided->move(sf::Vector2f(0, temp_bounds.top + temp_bounds.height - inter.top));
+			}
+			else if (pos.y <= inter.top) {
+				//(0,axis.y*(other.top - (inter.top + inter.height)));
+				collided->move(sf::Vector2f(0, temp_bounds.top - (inter.top + inter.height)));
+			}
+
+			if (pos.x > inter.left) {
+				//getAnimatedSprite().move(axis.x*((other.left + other.width) - inter.left), 0);
+				collided->move(sf::Vector2f(temp_bounds.left + temp_bounds.width, 0));
+			}
+			else if (pos.x <= inter.left) {
+				//getAnimatedSprite().move(axis.x*(other.left - (inter.left + inter.width)),0);
+				collided->move(sf::Vector2f(temp_bounds.left - (inter.left + inter.width), 0));
+			}
+			DoSomethingOnCollision(collided);
+		}
+
+	}
+
+	sf::Rect<float> bounds() {
+		return rect.getGlobalBounds();
+	}
+
+
 };
 
-class NormalTile : public Tile{
+class NormalTile : public Tile {
 
 public:
-	NormalTile(float size, const sf::Color &color, const sf::Vector2f &init_pos) : Tile(size, color,init_pos) {}
-	void update() override {} //Do Nothing
-	void DoSomethingOnCollision(Entity* collided) override{}
+	NormalTile(float size, const sf::Color &color, const sf::Vector2f &init_pos) : Tile(size, color, init_pos) {}
+	void update(float dt) override {} //Do Nothing
+	void DoSomethingOnCollision(Entity* collided) override {}
 
 };
 
 class LavaTile : public Tile {
 public:
 	LavaTile(float size, const sf::Color &color, const sf::Vector2f &init_pos) : Tile(size, color, init_pos) {}
-	void update() override {} //Do Nothing
+	void update(float dt) override {}
 	void DoSomethingOnCollision(Entity* collided) override {}
 };
+
 
 
 class Character : public Entity {
@@ -123,6 +132,7 @@ private:
 	int str, agi, intel, pdef, mdef, currhealth, maxhealth;
 	sf::Vector2f vel;
 	sf::Vector2f acc;
+
 public:
 	virtual void Attack() = 0;
 	virtual void SAttack() = 0;
@@ -146,11 +156,65 @@ public:
 
 
 
+class EntityManager {
+public:
+	std::vector<Tile*> map;
+	std::vector<Character*> players;
+	std::vector<Entity*> sobjects;
+
+	void addPlayer(Entity* p) {
+		players.push_back(dynamic_cast<Character*>(p));
+	}
+
+	void addMapTile(Entity* t) {
+		map.push_back(dynamic_cast<Tile*>(t));
+	}
+
+	void addSObject(Entity* so) {
+		sobjects.push_back(so);
+	}
+
+	void handleInput() {
+		
+	}
+
+	void update(float dt) {
+
+	}
+
+	void logic() {
+		for (auto e : map) {
+			for (auto c : players) {
+				e->resolveColision(c);
+			}
+		}
+	}
+
+	void render(sf::RenderTarget &g) {
+		for (auto e : map) {
+			e->render(g);
+		}
+		for (auto e : players) {
+			e->render(g);
+		}
+		for (auto e : sobjects) {
+			e->render(g);
+		}
+	}
+
+
+}em;
+
+
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Fight Me");
 	sf::Clock clock;
 	sf::Time lag = sf::seconds(0);
+	
+
+
+
 	for (int i = 0; i < WINDOW_WIDTH / TILE_SIZE; i++) {
 		em.addMapTile(new NormalTile(TILE_SIZE,sf::Color::Magenta,sf::Vector2f(i * TILE_SIZE,WINDOW_HEIGHT - TILE_SIZE)));
 	}
@@ -164,14 +228,20 @@ int main() {
 		}
 
 		//input
-		character->handleInput();
+		em.handleInput();
 		//update entities
-		em.update();
+		em.update(SPF.asSeconds());
 		//collision detection
 		em.logic();
 		//render
 		window.clear();
-		em.render(window);
+		sf::Texture tex;
+		sf::Sprite img;
+		if (tex.loadFromFile("sample.png")) {
+			img.setTexture(tex);
+			window.draw(img);
+		}
+		//em.render(window);
 		window.display();
 
 		const auto elapsed = clock.getElapsedTime() + lag;
