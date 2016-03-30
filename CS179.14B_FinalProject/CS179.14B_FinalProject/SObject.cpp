aeeb181a64227712cc198a22d05d60db346345e0
Renard <janrenardcalalang@yahoo.com>
2016-03-30 09:04:31 +0800

    Added SOBject(but have yet to finalize. Please don't touch yet.)

---
+++ CS179.14B_FinalProject/CS179.14B_FinalProject/SObject.cpp
@@ -0,0 +1,101 @@
+#include "Tile.h"
+#include "Character.h"
+void Tile::resolveColision(Entity* collided) {
+	sf::Rect<float> inter;
+	if (bounds().intersects(collided->bounds(), inter)) {
+		sf::Rect<float> temp_bounds = collided->bounds();
+		bool colX = false;
+		bool colY = false;
+		if (getPosition().y > inter.top) {
+			collided->move(sf::Vector2f(0, -inter.height));
+			colY = true;
+		}
+		else if (getPosition().y <= inter.top) {
+			collided->move(sf::Vector2f(0, inter.height));
+			colY = true;
+		}
+		/*
+		if (getPosition().x > inter.left) {
+		//getAnimatedSprite().move(axis.x*((other.left + other.width) - inter.left), 0);
+		cout << "COLLIDED 3\n";
+		collided->move(sf::Vector2f(temp_bounds.left + temp_bounds.width, 0));
+		colX = true;
+		}
+		else if (getPosition().x <= inter.left) {
+		//getAnimatedSprite().move(axis.x*(other.left - (inter.left + inter.width)),0);
+		cout << "COLLIDED 4\n";
+		collided->move(sf::Vector2f(temp_bounds.left - (inter.left + inter.width), 0));
+		}
+		collided->setCollision(colX, colY);
+		DoSomethingOnCollision(collided);
+		*/
+		
+		if (auto e = dynamic_cast<Character*>(collided)) {
+			e->resetGravity();
+			cout << "COLLIDE Y" << endl;
+		}
+	}
+	else {
+		collided->setCollision(false, false);
+	}
+
+}
+
+//added 9:12 pm 25/03/2016
+
+
+void NormalTile::update(float dt) {}
+void NormalTile::DoSomethingOnCollision(Entity* collided) {
+	//NOTHING? WHAT DOES HAPPEN HERE?
+}
+
+void LavaTile::update(float dt) {
+	timer += dt;
+	
+}
+void LavaTile::DoSomethingOnCollision(Entity* collided){
+	if (auto e = dynamic_cast<Character*>(collided)) {
+			timer = 0;
+			if (timer == 0){
+				e->takeDamage(5);
+			}
+			if(timer >= 2){
+				timer = 0;
+			}
+		}
+}
+
+void DestructibleTile::update(float dt) {
+	
+}
+void DestructibleTile::DoSomethingOnCollision(Entity* collided){
+	tileDurability--;
+	if(tileDurabilty <= 0){
+		toDelete = true;
+	}
+}
+
+void TrampolineTile::update(float dt) {}
+void TrampolineTile::DoSomethingOnCollision(Entity* collided){
+	if (auto e = dynamic_cast<Character*>(collided)) {
+			e->boosted();
+		}
+}
+
+void TarTile::update(float dt) {}
+void TarTile::DoSomethingOnCollision(Entity* collided){
+	if (auto e = dynamic_cast<Character*>(collided)) {
+			e->slow();
+		}
+	
+}
+/*
+void TeleportationTile::update(float dt) {}
+void TeleportationTile::DoSomethingOnCollision(Entity* collided){}
+
+void TrapTile::update(float dt) {}
+void TrapTile::DoSomethingOnCollision(Entity* collided){}
+
+void PermeableTile::update(float dt) {}
+void PermeableTile::DoSomethingOnCollision(Entity* collided){}
+*/
