4869b57faa940bc549f040505289faf9cd5678c8
Anemosian <Anemosian@users.noreply.github.com>
2016-04-05 11:45:56 +0800

    Trial edit to use attack via weapon

--- CS179.14B_FinalProject/CS179.14B_FinalProject/Weapon.cpp
+++ CS179.14B_FinalProject/CS179.14B_FinalProject/Weapon.cpp
@@ -2,3 +2,25 @@
 
 void Weapon::update(float dt){}
 void Weapon::fire(sf::Vector2f player_pos){}
+void Weapon::handleMouse(int key,sf::RenderWindow &win) {
+	if (key == 1) {
+		//Attack();
+		if (sf::Mouse::getPosition(win).x > 0 && sf::Mouse::getPosition(win).x <= GAME::WINDOW_WIDTH/2) {
+			cout << "Left Normal" << endl;
+		}
+		else if (sf::Mouse::getPosition(win).x >  GAME::WINDOW_WIDTH/2 && sf::Mouse::getPosition(win).x <= GAME::WINDOW_WIDTH) {
+			cout << "Right Normal" << endl;
+		}
+		key = 0;
+	}
+	if (key == 2) {
+		//SAttack();
+		if (sf::Mouse::getPosition(win).x > 0 && sf::Mouse::getPosition(win).x <= GAME::WINDOW_WIDTH / 2) {
+			cout << "Left Special" << endl;
+		}
+		else if (sf::Mouse::getPosition(win).x >  GAME::WINDOW_WIDTH / 2 && sf::Mouse::getPosition(win).x <= GAME::WINDOW_WIDTH) {
+			cout << "Right Special" << endl;
+		}
+		key = 0;
+	}
+}
