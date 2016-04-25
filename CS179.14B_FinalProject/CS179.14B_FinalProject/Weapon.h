7ba0e795b218baa1ff7165a5fb025bdcdae9051c
Renard <janrenardcalalang@yahoo.com>
2016-04-25 22:49:53 +0800

    Revert "Added stuff for sprite animation"
    This reverts commit 10409ed3a9fbc30a5ec2281b713d420159ea3290.

--- CS179.14B_FinalProject/CS179.14B_FinalProject/Weapon.h
+++ CS179.14B_FinalProject/CS179.14B_FinalProject/Weapon.h
@@ -1,17 +1,12 @@
 #pragma once
 #include "Entities.h"
 class Weapon : public Entity{
-private:
-enum Face {
-	UP, MID, DOWN, FTHROUGH, NONE
-};
-	Face currface;
+
 public:
 	Weapon(string file_name, int weapon_width, int weapon_height) : Entity(file_name, sf::Vector2i(weapon_width, weapon_height)){}
 	~Weapon(){}
 	virtual void fire(sf::Vector2f player_pos) = 0;
 	void update(float dt) override;
-	currface = Face::NONE;
 };
 
 class Sword : public Weapon {
